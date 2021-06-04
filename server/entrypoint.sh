681f5d9826b81224c6810ed84eefcfcb1f8c93f6
RuiSanches <rf.sanches@hotmail.com>
2021-06-05 00:42:34 +0100

    Adds clean env with a deno executable for production build stage

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,7 +3,10 @@ set -e
 
 case "$1" in
 bundle | cache | compile | completions | coverage | doc | eval | fmt | help | info | install | lint | lsp | repl | run | test | types | upgrade)
-    if [ "${BUILD_MODE}" = 'development' ]; then
+    if [ "${BUILD_MODE}" = 'production' ]; then
+        echo 'Starting compiled deno app'
+        ./app
+    elif [ "${BUILD_MODE}" = 'development' ]; then
         echo 'Starting denon'
         exec denon "$@"
     else
