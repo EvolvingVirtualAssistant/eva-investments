77e182d526dba11ecd26a1c64d5f8a69d734d27a
PedroSanches <pm.sanches@hotmail.com>
2021-07-21 00:45:15 +0100

    Adds DOCKER_BUILDKIT to ensure that only the required stages are built instead of building all stages until the target one

--- jenkins/server_ci.sh
+++ jenkins/server_ci.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --build-arg DEFAULT_BUILD_MODE=continuous-integration -t eva-investments-server-debian --target continuous-integration server/
+DOCKER_BUILDKIT=1 docker build -t eva-investments-server-debian --target continuous-integration server/
