77d0e254e1f5c9d0d0eb4c26045e5cb348f39a14
RuiSanches <rf.sanches@hotmail.com>
2021-08-14 23:36:40 +0100

    Prepares deno server folder structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,38 @@
-import { pathJoin, ROOT_PATH, serve } from "./deps.ts";
-import { dotEnvConfig } from "./deps.ts";
-import "https://deno.land/x/dotenv/load.ts";
-import { execute } from "./libs/swagger-client-mapper/swaggerClient.ts";
+import { pathJoin, ROOT_PATH, serve } from './deps.ts';
+import { dotEnvConfig } from './deps.ts';
+import 'https://deno.land/x/dotenv/load.ts';
+import { execute } from './libs/swagger-client-mapper/swaggerClient.ts';
+import { sleep } from './utils/async.ts';
+import { RootCliAdapter } from './rootCliAdapter.ts';
+import { WalletsCliAdapter } from './wallets/drivers/walletsCliAdapter.ts';
 
-console.log("Loading env file...");
+console.log('Loading env file...');
 console.log(
   dotEnvConfig({
     export: true,
     safe: true,
-    path: pathJoin(ROOT_PATH, "/resources/env/.env"),
-    example: pathJoin(ROOT_PATH, "/resources/env/.env.required_keys"),
-  }),
+    path: pathJoin(ROOT_PATH, '/resources/env/.env'),
+    example: pathJoin(ROOT_PATH, '/resources/env/.env.required_keys'),
+  })
 );
 
-await execute();
+//await execute();
 
-const PORT = 1993;
-const s = serve(`0.0.0.0:${PORT}`);
-const body = new TextEncoder().encode("Hello World\n");
-
-console.log("EVA Investments Server");
-console.log(`Server started on port ${PORT}`);
-for await (const req of s) {
-  req.respond({ body });
+// Init all cliAdapters
+function initCliAdapters() {
+  return {
+    rootCliAdapter: new RootCliAdapter(),
+    walletsCliAdapter: new WalletsCliAdapter(),
+  };
 }
+
+async function main() {
+  // deno-lint-ignore no-unused-vars
+  const cliAdapters = initCliAdapters();
+
+  while (true) {
+    await sleep(10000);
+  }
+}
+
+await main();
--- server/src/deps.ts
+++ server/src/deps.ts
@@ -1,28 +1,28 @@
-export { serve } from "https://deno.land/std@0.77.0/http/server.ts";
-import { join as pathJoin } from "https://deno.land/std@0.108.0/path/mod.ts";
+export { serve } from 'https://deno.land/std@0.77.0/http/server.ts';
+import { join as pathJoin } from 'https://deno.land/std@0.108.0/path/mod.ts';
 export { pathJoin };
 
 // config
-export type { DenonConfig } from "https://deno.land/x/denon@2.4.7/mod.ts";
-import { config as dotEnvConfig } from "https://deno.land/x/dotenv/mod.ts";
+export type { DenonConfig } from 'https://deno.land/x/denon@2.4.7/mod.ts';
+import { config as dotEnvConfig } from 'https://deno.land/x/dotenv/mod.ts';
 export { dotEnvConfig };
 
 // libs
-import SwaggerClient from "../src/libs/swagger-client-v3_15_0.js"; //v3.15.0 -> "https://cdn.skypack.dev/swagger-client";
+import SwaggerClient from '../src/libs/swagger-client-v3_15_0.js'; //v3.15.0 -> "https://cdn.skypack.dev/swagger-client";
 export { SwaggerClient };
-export { getApiSignature } from "../src/libs/api-signature/mod.ts";
-export type { Sign } from "../src/libs/api-signature/mod.ts";
+export { getApiSignature } from '../src/libs/api-signature/mod.ts';
+export type { Sign } from '../src/libs/api-signature/mod.ts';
 
 // tests
 export {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.106.0/testing/asserts.ts";
+} from 'https://deno.land/std@0.106.0/testing/asserts.ts';
 
 // path config
 const currentWorkingDir = Deno.cwd();
 export const ROOT_PATH = findRootFolder(currentWorkingDir);
-console.log("ROOT_PATH", ROOT_PATH);
+console.log('ROOT_PATH', ROOT_PATH);
 
 function findRootFolder(path: string): string {
   let foldersMatched = 0;
@@ -32,14 +32,15 @@ function findRootFolder(path: string): string {
       return path;
     }
 
-    if (dirEntry.name === "src" || dirEntry.name === "tests") {
+    if (dirEntry.name === 'src' || dirEntry.name === 'tests') {
       foldersMatched++;
     } else if (
-      dirEntry.name === "libs" || dirEntry.name === "unit" ||
-      dirEntry.name === "integration"
+      dirEntry.name === 'libs' ||
+      dirEntry.name === 'unit' ||
+      dirEntry.name === 'integration'
     ) {
-      return findRootFolder(path.substr(0, path.lastIndexOf("/")));
-    } else if (dirEntry.name === "server" || dirEntry.name === "app") {
+      return findRootFolder(path.substr(0, path.lastIndexOf('/')));
+    } else if (dirEntry.name === 'server' || dirEntry.name === 'app') {
       return findRootFolder(pathJoin(path, dirEntry.name));
     }
   }
@@ -49,5 +50,5 @@ function findRootFolder(path: string): string {
     return path;
   }
 
-  throw new Error("Could not find root folder");
+  throw new Error('Could not find root folder');
 }
